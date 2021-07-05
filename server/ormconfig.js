e632d30e4a05cc40aa413092a9850a63286e67b5
supraja <suprajaa.raman@gmail.com>
2021-07-05 22:21:31 +0530

    added database url

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -7,6 +7,13 @@ module.exports = {
   database: 'notes',
   synchronize: true,
   logging: true,
+  url: process.env.DATABASE_URL,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   entities: ['build/entity/*.js'],
   migrations: ['build/migration/*.js'],
   cli: {
