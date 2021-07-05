032bab589749ab19ad77f2ee254f04ae6bc6bb11
supraja <suprajaa.raman@gmail.com>
2021-07-05 22:30:13 +0530

    fixed dbconfig

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,9 +1,5 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
   database: 'notes',
   synchronize: true,
   logging: true,
