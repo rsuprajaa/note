94d704d0dfe1d005f4dc33b238cbedb495f4f8c7
supraja <suprajaa.raman@gmail.com>
2021-07-15 19:38:07 +0530

    fixed cors error

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,13 +16,12 @@ const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: '*',
+    origin: process.env.ORIGIN,
     optionsSuccessStatus: 200,
   })
 )
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
